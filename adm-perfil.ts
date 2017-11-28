da48b2f919b26efa0f0675ba8420d926e1f2711b
Guillermo Fink <guillermo.fink@bsg.global>
2017-11-28 18:34:02 -0300

    Add files via upload
    ADM-Perfil PPS

---
+++ adm-perfil.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MainPage } from '../main/main';
+
+/**
+ * Generated class for the AdmPerfilPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-adm-perfil',
+  templateUrl: 'adm-perfil.html',
+})
+export class AdmPerfilPage {
+
+  id: number;
+  nombre: string;
+  apellido: string;
+  mail: string;
+  password: string;
+  legajo: number;
+  tipo: number;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.id = this.navParams.get('id');
+    this.nombre = this.navParams.get('nombre');
+    this.apellido = this.navParams.get('apellido');
+    this.mail = this.navParams.get('mail');
+    this.password = this.navParams.get('password');
+    this.legajo = this.navParams.get('legajo');
+    this.tipo = this.navParams.get('tipo');
+  }
+
+  Volver() {
+    this.navCtrl.setRoot(MainPage, {
+      "id": this.id,
+      "nombre": this.nombre,
+      "apellido": this.apellido,
+      "mail": this.mail,
+      "password": this.password,
+      "legajo": this.legajo,
+      "tipo": this.tipo
+    });
+  }
+
+}
